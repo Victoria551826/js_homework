cf0cc5655be0fb04a49f635e77bcde56c4ce23aa
Victoria P <vi4ka551@gmail.com>
2024-11-10 22:44:16 +0000

    Home work 4

--- homework.js
+++ homework.js
@@ -1,9 +1,17 @@
-function pow(x,y)
+function checkProbabilityTheory(count)
 {
-    let result = 1;
-    for (let i = 1; i <= y; i++) {
-       result = result * x;
-      }
-      return result;
+    let evenCount = 0;
+    const min = 100;
+    const max = 1001;
+    for (let i = 1; i <= count; i++) {
+        const num = Math.floor(Math.random() * (max - min)) + min;
+        if (num % 2 == 0) {
+            evenCount++;
+        }
+    }
+    console.log('Number of generated: ', count);
+    console.log('Even count: ', evenCount);
+    console.log('Odd count: ', count - evenCount);
+    console.log('Even %: ', (evenCount / count) * 100);
 }
-console.log(pow(2,3));
+checkProbabilityTheory(999);
